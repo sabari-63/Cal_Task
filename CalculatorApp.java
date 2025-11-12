a9489373070ebdd7ff74f36413ac0024d2aebce8
sabari-63 <srinivassabari63@gmail.com>
2025-11-11 21:43:30 -0800

    Add files via upload

---
+++ CalculatorApp.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueCalculation = true;
+
+        System.out.println("===== Simple Calculator =====");
+
+        while (continueCalculation) {
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.print("Enter an operator (+, -, *, /, %): ");
+            char operator = scanner.next().charAt(0);
+
+            double result = 0;
+            boolean validOperation = true;
+
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Error! Division by zero is not allowed.");
+                        validOperation = false;
+                    }
+                    break;
+                case '%':
+                    result = num1 % num2;
+                    break;
+                default:
+                    System.out.println("Invalid operator!");
+                    validOperation = false;
+            }
+
+            if (validOperation) {
+                System.out.println("Result: " + result);
+            }
+
+            System.out.print("Do you want to perform another calculation? (yes/no): ");
+            String choice = scanner.next();
+
+            if (!choice.equalsIgnoreCase("yes")) {
+                continueCalculation = false;
+                System.out.println("Thank you for using the calculator!");
+            }
+        }
+
+        scanner.close();
+    }
+}
